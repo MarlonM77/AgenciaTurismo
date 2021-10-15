176603e601aeaed771f50f4760010492f4a9cf08
Marlon <marlondeep77@gmail.com>
2021-10-15 11:27:44 -0500

    deployment testings2

--- authApp/serializers/planSerializer.py
+++ authApp/serializers/planSerializer.py
@@ -8,6 +8,23 @@ class PlanSerializer(serializers.ModelSerializer):
         model = Plan
         fields = ['valor','fecha_inicio','fecha_fin', 'nombre_plan', 'descripcion', 'cant_personas']
     
+    def to_representation(self, obj):
+        user = User.objects.get(id = obj.user)
+        plan = Plan.objects.get(id = obj.id)
+
+        return{
+            'valor':           plan.valor,
+            'fecha_inicio':    plan.fecha_inicio,
+            'fecha_fin':       plan.fecha_fin,
+            'nombre_plan':     plan.nombre_plan,
+            'descripcion':     plan.descripcion,
+            'cant_personas':   plan.cant_personas,
+            'user': {
+                'username':   user.username,
+                'name':       user.name,
+                'email':      user.email
+            }
+        }
 
 
 
